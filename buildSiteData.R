a4de1bdef41f9e4f6434150dacaab55554499345
John Everett <everett.jk@gmail.com>
2025-03-21 16:51:33 +0000

    Added nearest gene

--- buildSiteData.R
+++ buildSiteData.R
@@ -6,16 +6,16 @@ library(RMySQL)
 # ---------------------------------------------------------------------------------------------------------------------------
 mergedData <- bind_rows(lapply(list.files('data', pattern = 'intSites.csv', recursive = TRUE, full.names = TRUE), read_csv))
 mergedData$patient <- toupper(mergedData$patient)
-mergedData <- select(mergedData, patient, seqnames, start, strand, dataSource, GTSP, cellType, timePoint, reads, estAbund, relAbund)
-names(mergedData) <- c('patient', 'seqnames', 'position', 'strand', 'dataSource', 'internalSampleID', 'cellType', 'timePoint', 'reads', 'estAbund', 'relAbund')
+mergedData <- select(mergedData, patient, seqnames, start, strand, dataSource, GTSP, cellType, timePoint, reads, estAbund, relAbund, nearestFeature, nearestFeatureDist)
+names(mergedData) <- c('patient', 'seqnames', 'position', 'strand', 'dataSource', 'internalSampleID', 'cellType', 'timePoint', 'reads', 'estAbund', 'relAbund', 'nearestFeature', 'nearestFeatureDist')
 
 
 illuminaOnlyData <- read_tsv('data/intSites.tsv', show_col_types = FALSE)
 illuminaOnlyData$patient <- toupper(illuminaOnlyData$subject)
 illuminaOnlyData <- subset(illuminaOnlyData, ! patient %in% mergedData$patient)
 illuminaOnlyData$dataSource <- 'Illumina'
-illuminaOnlyData <- select(illuminaOnlyData, patient, chromosome, position, strand, dataSource, internalSampleID, cellType, timePoint, reads, estAbund, relAbund)
-names(illuminaOnlyData) <- c('patient', 'seqnames', 'position', 'strand', 'dataSource', 'internalSampleID', 'cellType', 'timePoint', 'reads', 'estAbund', 'relAbund')
+illuminaOnlyData <- select(illuminaOnlyData, patient, chromosome, position, strand, dataSource, internalSampleID, cellType, timePoint, reads, estAbund, relAbund, nearestFeature, nearestFeatureDist)
+names(illuminaOnlyData) <- c('patient', 'seqnames', 'position', 'strand', 'dataSource', 'internalSampleID', 'cellType', 'timePoint', 'reads', 'estAbund', 'relAbund', 'nearestFeature', 'nearestFeatureDist')
 
 sites <- bind_rows(mergedData, illuminaOnlyData)
 sites$posid <- paste0(sites$seqnames, sites$strand, sites$position)
